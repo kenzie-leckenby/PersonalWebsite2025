727cb0886a7bbcbec33beee5fd01fd47b9399805
Kezie <Leckenby.kenzie@gmail.com>
2024-11-12 14:33:57 -0500

    Implemented a visual tester for blogs

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -2,13 +2,25 @@ import * as React from 'react';
 import { Link } from '@remix-run/react';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import BlogThumbnail from '~/src/blogThumbnail';
+import { Container } from '@mui/material';
+import darkTheme from '~/src/theme';
 
 export default function About() {
   return (
     <React.Fragment>
-      <Typography variant="h2" component="h1" sx={{ mt: 2, fontWeight: 500}}>
-        Welcome to my blog!
-      </Typography>
+      <Container maxWidth='xl' disableGutters>
+        <Typography variant="h3" component="h1" sx={{ 
+          mt: 2, 
+          pb: 2,
+          mb: 4, 
+          fontWeight: 500,
+          borderBottom: `2px solid ${darkTheme.palette.divider}`
+        }}>
+          Welcome to my blog!
+        </Typography>
+        <BlogThumbnail title='New Blog Post' date='2:08pm - November 12, 2024' synopsis='Wow I be yappin' tags={['C#', 'Yap']}></BlogThumbnail>
+      </Container>
     </React.Fragment>
   );
 }
